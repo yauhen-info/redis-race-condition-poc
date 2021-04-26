e055479e5fcbf1188baeed502c91e0f11753c6af
yauhen-info <yauhen@gistik.com>
2021-04-26 23:43:10 +0300

    remove echoed text from db_updater job script to minimize log output

--- docker/db_updater/update_db_job.sh
+++ docker/db_updater/update_db_job.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 while true; do
-  echo "Trying to update score_db database!"
   mysql -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} -e "UPDATE scores SET counter = counter+1 WHERE name = 'game_1_player_1'"
   mysql -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} -e "select name, counter from scores WHERE name = 'game_1_player_1'"
   sleep 5
