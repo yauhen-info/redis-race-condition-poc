0dc7d1b805c54eeeb5644e36161963cfef6c040d
yauhen-info <yauhen@gistik.com>
2021-04-28 00:56:12 +0300

    change database data tp fill at start; update environment variables accordingly

--- docker/db/setup.sql
+++ docker/db/setup.sql
@@ -2,9 +2,10 @@ use score_db;
 create table scores
 (
     name    varchar(64),
-    counter int(3)
+    counter int
 );
-insert into scores
-values ('game_1_player_1', 1);
-insert into scores
-values ('game_1_player_2', 1);
+insert into scores values ('game_1', 1);
+insert into scores values ('game_2', 1);
+insert into scores values ('game_3', 1);
+insert into scores values ('game_4', 1);
+insert into scores values ('game_5', 1);
--- docker/db_updater/update_db_job.sh
+++ docker/db_updater/update_db_job.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+MYSQL_DATABASE=score_db
 while true; do
-  mysql -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} -e "UPDATE scores SET counter = counter+1 WHERE name = 'game_1_player_1'"
-  mysql -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} -e "select name, counter from scores WHERE name = 'game_1_player_1'"
+  for j in 1 2 3 4 5; do
+    SQL_UPDATE_CMD="UPDATE scores SET counter = counter+1 WHERE name = 'game_$j';"
+    mysql ${MYSQL_DATABASE} -e "$SQL_UPDATE_CMD"
+
+    SQL_SELECT_CMD="SELECT name, counter FROM scores WHERE name = 'game_$j';"
+    mysql ${MYSQL_DATABASE} -t -e "$SQL_SELECT_CMD"
+  done
   sleep 5
 done
