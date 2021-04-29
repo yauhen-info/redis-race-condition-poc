cc4dd37a09502535b2dcb9e7782eee4f9cc589e9
yauhen-info <yauhen@gistik.com>
2021-04-30 02:55:16 +0300

    add db_updater_service to a start of services list

--- start_project.sh
+++ start_project.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # the job shall spin up all the services excluding those used for testing
 cd docker || exit
-docker-compose build --parallel --remove-orphans
-docker-compose up --scale rest-service=3 nginx
+docker-compose up --scale rest-service=3 nginx db_updater_service
